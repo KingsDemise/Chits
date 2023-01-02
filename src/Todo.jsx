6aadfebb073fc6d9697279b5d5feeb1cf4d4eb08
Kingshuk Kirtania <bttb.kingshukk@gmail.com>
2023-01-02 20:02:32 +0530

    setup gh-pages

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {ImCross} from "react-icons/im"
 const style={
-    li: `flex justify-between bg-[#ffe9ff] p-4 my-2 capitalize rounded-md`,
+    li: `flex justify-between bg-[#ffe9ff] p-4 my-2 rounded-md`,
     row: `flex`,
     text: `text-white font-Montserrat ml-2 cursor-pointer`,
     button: `flex items-center`,
